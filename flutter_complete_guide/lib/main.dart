0e0cdb2db5de4c4bb08b337a061d8d4685eef281
Dilpreet <n.dilpreetsingh@gmail.com>
2023-03-09 23:10:09 +0530

    converted stateless widget to statefull so that we can change question on screen and movedText widget to new file

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionInd = 0;
+  var questionsArr = ["question one?", "question two?"];
+
+  void answerQuestion(int num) {
+    if (questionInd == questionsArr.length - 1) {
+      return;
+    }
+    print("Answer Chosen $num");
+    setState(() {
+      questionInd++;
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My App"),
         ),
-        body: Text("Hello guys welcome to my app"),
+        body: Column(
+          children: [
+            Question(questionsArr[questionInd]),
+            ElevatedButton(
+              onPressed: () {
+                answerQuestion(1);
+              },
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                answerQuestion(2);
+              },
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                answerQuestion(3);
+              },
+              child: Text("Answer 3"),
+            ),
+          ],
+        ),
       ),
     );
   }
