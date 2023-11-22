a9c220b3538ad70531d46b7740dace166c2f338c
Dilpreet <n.dilpreetsingh@gmail.com>
2023-11-22 22:46:47 +0530

    #expense_tracker - commented code for locking orientation of device

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+// import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -10,6 +11,11 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then(
+  //   (fn) =>
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -44,5 +50,6 @@ void main() {
               )),
       home: const Expenses(),
     ),
+    // ),
   );
 }
