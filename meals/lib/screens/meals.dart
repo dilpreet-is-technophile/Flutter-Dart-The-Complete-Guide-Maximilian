500a36e5e0ee2185ca0a0d673799814d38c46eb6
Dilpreet <n.dilpreetsingh@gmail.com>
2023-12-07 23:12:25 +0530

    #meals - added a bottom navigation tab bar and favourite meals screen

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -6,16 +6,21 @@ import 'package:meals/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
+    required this.onToggleFavourite,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavourite;
 
   void selectMeal(BuildContext context, meal) {
-    Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => MealDetailsScreen(meal: meal)));
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => MealDetailsScreen(
+              meal: meal,
+              onToggleFavourite: onToggleFavourite,
+            )));
   }
 
   @override
@@ -54,9 +59,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
