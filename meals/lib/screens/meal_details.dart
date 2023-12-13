7e84324212babd121c82bc28cf62a562215fd8ae
Dilpreet <n.dilpreetsingh@gmail.com>
2023-12-14 00:04:38 +0530

    #meals - implicit animation on favoriteicon

--- meals/lib/screens/meal_details.dart
+++ meals/lib/screens/meal_details.dart
@@ -20,17 +20,29 @@ class MealDetailsScreen extends ConsumerWidget {
         title: Text(meal.title),
         actions: [
           IconButton(
-            onPressed: () {
-              final wasAdded = ref
-                  .read(favoriteMealsProvider.notifier)
-                  .toggleMealFavouriteStatus(meal);
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text(
-                      wasAdded ? "Meal added as favourite" : "Meal removed")));
-            },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
-          )
+              onPressed: () {
+                final wasAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleMealFavouriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(wasAdded
+                        ? "Meal added as favourite"
+                        : "Meal removed")));
+              },
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: ((child, animation) {
+                  return RotationTransition(
+                    turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                    child: child,
+                  );
+                }),
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ))
         ],
       ),
       body: SingleChildScrollView(
