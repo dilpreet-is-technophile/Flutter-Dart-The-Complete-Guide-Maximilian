a8df5bc0fb65affbaee5fcaf5b559848a83eaa46
Dilpreet <n.dilpreetsingh@gmail.com>
2023-12-20 23:57:07 +0530

    #shopping_list - code for new item page

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -30,6 +30,7 @@ class _NewItemState extends State<NewItem> {
         body: Padding(
           padding: const EdgeInsets.all(12),
           child: Form(
+            key: _formkey,
             child: Column(
               children: [
                 TextFormField(
@@ -38,7 +39,16 @@ class _NewItemState extends State<NewItem> {
                     label: Text("Name"),
                   ),
                   validator: (value) {
-                    return "Demo...";
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return "Must be between 1 and 50 characters.";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _enteredName = value!;
                   },
                 ),
                 Row(
@@ -48,30 +58,66 @@ class _NewItemState extends State<NewItem> {
                         decoration: const InputDecoration(
                           label: Text("Quantity"),
                         ),
-                        initialValue: "1",
+                        keyboardType: TextInputType.number,
+                        initialValue: _enteredQuantity.toString(),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              int.tryParse(value) == null ||
+                              int.tryParse(value)! <= 0) {
+                            return "Must be a valid positive number";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _enteredQuantity = int.parse(value!);
+                        },
                       ),
                     ),
                     const SizedBox(width: 8),
                     Expanded(
-                      child: DropdownButtonFormField(items: [
-                        for (final category in categories.entries)
-                          DropdownMenuItem(
-                            value: category.value,
-                            child: Row(
-                              children: [
-                                Container(
-                                  width: 16,
-                                  height: 16,
-                                  color: category.value.color,
-                                ),
-                                const SizedBox(
-                                  width: 6,
-                                ),
-                                Text(category.value.title)
-                              ],
+                      child: DropdownButtonFormField(
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: _selectedCategory,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.value.color,
+                                  ),
+                                  const SizedBox(
+                                    width: 6,
+                                  ),
+                                  Text(category.value.title)
+                                ],
+                              ),
                             ),
-                          )
-                      ], onChanged: (value) {}),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedCategory = value!;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        _formkey.currentState!.reset();
+                      },
+                      child: const Text("Reset"),
+                    ),
+                    ElevatedButton(
+                      onPressed: _saveItem,
+                      child: const Text("Add Item"),
                     )
                   ],
                 )
