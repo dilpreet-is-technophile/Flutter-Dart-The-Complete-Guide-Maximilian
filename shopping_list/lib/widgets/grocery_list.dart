2a3f065ce00a080f76ceb57bbe17440508aaeb1f
Dilpreet <n.dilpreetsingh@gmail.com>
2023-12-24 21:56:50 +0530

    #shopping_list - error handling and displaying a default message when error occurs

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -17,6 +17,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -29,6 +30,12 @@ class _GroceryListState extends State<GroceryList> {
         "flutter-course-udemy-6cbab-default-rtdb.asia-southeast1.firebasedatabase.app",
         "shopping-list.json");
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = "Failed to fetch data please try again later";
+      });
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -101,6 +108,11 @@ class _GroceryListState extends State<GroceryList> {
         ),
       );
     }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
